9a943461edc30985b0eee6325151f6903738576e
MauriceLee <ee.maurice.lee@gmail.com>
2019-10-08 18:27:57 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App'
 import router from './router'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
+
+axios.defaults.withCredentials = true
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +19,22 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    const api = `${process.env.APIPATH}/api/user/check`
+    axios.post(api).then(res => {
+      console.log(res.data)
+      if (res.data.success === true) {
+        next()
+      } else {
+        next({
+          path: '/login'
+        })
+      }
+    })
+  } else {
+    next()
+  }
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/pages/Login'
+import Admin from '@/components/Admin'
+import Products from '@/components/pages/Products'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '*',
+      redirect: 'login'
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: Admin,
+      children: [
+        {
+          path: 'products',
+          name: 'products',
+          component: Products,
+          meta: { requiresAuth: true }
+        }
+      ]
     }
   ]
 })
